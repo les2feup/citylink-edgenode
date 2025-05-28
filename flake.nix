43bba7a2795e91a9b26bf867400396577d1b9b2a
dvalinn <tiago.andre.amorim@gmail.com>
2025-05-28 15:42:49 +0100

    chore: added .gitignore, and nix shell flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Micropython dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodePackages_latest.prettier # code formatter
+            deno # dev tools and runtime
+          ];
+        };
+      }
+    );
+}
