963528dea8b4de54bda0092827b6ba53a5bfbee8
dvalinn <tiago.andre.amorim@gmail.com>
2025-06-06 15:01:33 +0100

    feat: helper scripts to open/close mqtt port on firewall

--- flake.nix
+++ flake.nix
@@ -16,6 +16,16 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        openfw = pkgs.writeShellScriptBin "openfw" ''
+          sudo iptables -A INPUT -p tcp --dport 1883 -j ACCEPT
+          sudo systemctl reload firewall
+        '';
+
+        closefw = pkgs.writeShellScriptBin "closefw" ''
+          sudo iptables -D INPUT -p tcp --dport 1883 -j ACCEPT
+          sudo systemctl reload firewall
+        '';
       in
       {
         devShells.default = pkgs.mkShell {
@@ -23,6 +33,9 @@
             nodePackages_latest.prettier # code formatter
             deno # dev tools and runtime
             mosquitto # MQTT broker
+
+            openfw
+            closefw
           ];
         };
       }
