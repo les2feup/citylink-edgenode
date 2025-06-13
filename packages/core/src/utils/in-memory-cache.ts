6229e3a204f1dc97c55192a9525c8c1c0c8710f5
dvalinn <tiago.andre.amorim@gmail.com>
2025-06-13 13:36:14 +0100

    feat: added getMap and getAll methods to cache interface

--- packages/core/src/utils/in-memory-cache.ts
+++ packages/core/src/utils/in-memory-cache.ts
@@ -7,6 +7,14 @@ export class InMemoryCache<K, V> implements Cache<K, V> {
     this.cache = new Map<K, V>();
   }
 
+  getMap(): Promise<ReadonlyMap<K, V>> {
+    return Promise.resolve(this.cache);
+  }
+
+  getAll(): Promise<ReadonlyArray<V>> {
+    return Promise.resolve(Array.from(this.cache.values()));
+  }
+
   get(key: K): Promise<V | undefined> {
     return Promise.resolve(this.cache.get(key));
   }
--- packages/core/src/utils/null-cache.ts
+++ packages/core/src/utils/null-cache.ts
@@ -1,6 +1,15 @@
 import type { Cache } from "../types/cache.ts";
 
 export class NullCache<K, V> implements Cache<K, V> {
+
+  getMap(): Promise<ReadonlyMap<K, V>> {
+    return Promise.resolve(new Map<K, V>());
+  }
+
+  getAll(): Promise<readonly V[]> {
+    return Promise.resolve([]);
+  }
+
   get(_key: K): Promise<V | undefined> {
     return Promise.resolve(undefined);
   }
